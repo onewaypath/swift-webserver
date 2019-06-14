31daefd515fe38485140c27f8d2bd394b9df031c
Alex Young <alexyoung@Alexs-IMac.local>
2019-06-14 16:12:39 -0400

    Revise landing page organization:
    - funding-01 on funding/01
    - funding-02 on funding/02
    - incorporation-01 on incorporation/02
    - incorporation-02 on incorporation/02
    
    Currently page 01 of a campaign landing page directs the user to a newsletter mailing list while page 02 asks the user to book an appointment directly.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,9 +7,22 @@ public func routes(_ router: Router) throws {
         return try req.view().render("welcome")
     }
     
-    router.get ("funding") { req in
-        return try req.view().render("funding")
+    router.get ("funding", "01") { req in
+        return try req.view().render("funding-01")
     }
+    
+    router.get ("funding", "02") { req in
+        return try req.view().render("funding-02")
+    }
+    
+    router.get ("incorporation", "01") { req in
+        return try req.view().render("incorporation-01")
+    }
+    
+    router.get ("incorporation", "02") { req in
+        return try req.view().render("incorporation-02")
+    }
+    
     // Says hello
     router.get("hello", String.parameter) { req -> Future<View> in
         return try req.view().render("hello", [
