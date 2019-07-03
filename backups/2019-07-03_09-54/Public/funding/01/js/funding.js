00cd4fa0f8cb56d4a1f795b7a901a3c06b8a071d
Alex Young <ayoung@onewaypath.com>
2019-07-03 10:28:15 -0400

    add thank you page

---
+++ backups/2019-07-03_09-54/Public/funding/01/js/funding.js
@@ -0,0 +1,193 @@
+(function ($) {
+    $(document).ready(function () {
+
+        $("#july").on('click',function () {
+            $("#june-month").hide();
+            $("#june-table").hide();
+            $("#july-month").show();
+            $("#july-table").show();
+            $("#july-table").show();
+            $('#june i').removeAttr('id');
+            $('#july i').attr('id', 'disabled');
+        });
+
+        $("#june").on('click',function () {
+            $("#july-month").hide();
+            $("#july-table").hide();
+            $("#june-month").show();
+            $("#june-table").show();
+            $('#july i').removeAttr('id');
+            $('#june i').attr('id', 'disabled');
+        });
+
+
+        // Select all links with hashes
+        $('a[href*="#"]')
+                // Remove links that don't actually link to anything
+                .not('[href="#"]')
+                .not('[href="#0"]')
+                .click(function (event) {
+                    // On-page links
+                    if (
+                            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                            &&
+                            location.hostname == this.hostname
+                            ) {
+                        // Figure out element to scroll to
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                        // Does a scroll target exist?
+                        if (target.length) {
+                            // Only prevent default if animation is actually gonna happen
+                            event.preventDefault();
+                            $('html, body').animate({
+                                scrollTop: target.offset().top
+                            }, 1000, function () {
+                                // Callback after animation
+                                // Must change focus!
+                                var $target = $(target);
+                                $target.focus();
+                                if ($target.is(":focus")) { // Checking if the target was focused
+                                    return false;
+                                } else {
+                                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                    $target.focus(); // Set focus again
+                                }
+                                ;
+                            });
+                        }
+                    }
+                });
+
+        $('a[href*=\\#]').bind('click', function (e) {
+            
+
+            if($(this).attr('data-target') == 'new'){
+                 
+            } else {
+                e.preventDefault(); // prevent hard jump, the default behavior
+                var target = $(this).attr("href");            
+                if (target == '#') {
+                    var offset = 0
+                } else {
+                    if ($(target).length) {
+                        var offset = $(target).offset().top;    
+                    } else{
+                        var offset = 0
+                    }
+                    if (target == '#home') {
+                        offset = 0
+                    }
+
+                    // perform animated scrolling by getting top-position of target-element and set it as scroll target
+                    $('html, body').stop().animate({
+                        scrollTop: offset
+                    }, 600, function () {
+                        //location.hash = target; //attach the hash (#jumptarget) to the pageurl
+                    });
+                }
+                return false;
+            }
+
+            
+            
+
+            
+        });
+
+        $(window).scroll(function () {
+            var scrollDistance = $(window).scrollTop();
+            $('[data-identifier="page-section"]').each(function (i) {
+                if ($(this).position().top <= scrollDistance) {
+                    $("nav a").removeAttr('data-active');
+                    $("nav a").eq(i).attr('data-active', '1');
+                }
+            });
+        }).scroll();
+
+        var wid = $(window).width();
+
+        $(window).resize(function () {
+            wid = $(window).width();
+            if (wid > 991) {
+                $("nav div").show();
+            }
+            else {
+                $("nav div").hide();
+            }
+        });
+
+        $("nav button").click(function () {
+            if (wid <= 991)
+            {
+                $("nav div").show("slow");
+                $(this).hide("slow");
+            }
+        });
+        $("nav a").click(function () {
+            if (wid <= 991)
+            {
+                $("nav div").hide("slow");
+                $("nav button").show("slow");
+            }
+        });
+        $("nav button").on('touchend', function () {
+            if (wid <= 991)
+            {
+                $("nav div").show("slow");
+                $(this).hide("slow");
+            }
+        });
+        $("nav a").on('touchend', function () {
+            if (wid <= 991)
+            {
+                $("nav div").hide("slow");
+                $("nav button").show("slow");
+            }
+        });
+
+        /*
+         $("section:nth-child(2) div a").click(function(){
+         $("section:nth-child(2) div blockquote").hide();
+         $("section:nth-child(2) div blockquote").eq($(this).index()).show();
+         });
+         $("section:nth-child(2) div a").on('touchend',function(){
+         $("section:nth-child(2) div blockquote").hide();
+         $("section:nth-child(2) div blockquote").eq($(this).index()).show();
+         });
+         var wid = $(window).width();
+         if (wid <= 767)
+         {
+         $("section:nth-child(2) div a").click(function(){
+         $("section:nth-child(2) h2 + p").hide();
+         });
+         $("section:nth-child(2) div a").on('touchend',function(){
+         $("section:nth-child(2) h2 + p").hide();
+         });
+         } 
+         
+         $("section:nth-child(3) dd figcaption ul li").click(function(){
+         $("section:nth-child(3) dd blockquote").hide();
+         $("section:nth-child(3) dd blockquote").eq($(this).index()).show();
+         });
+         
+         $("section:nth-child(3) dd figcaption ul li").on('touchend',function(){
+         $("section:nth-child(3) dd blockquote").hide();
+         $("section:nth-child(3) dd blockquote").eq($(this).index()).show();
+         });
+         
+         $("footer figcaption ul li a").click(function(){
+         console.log(this);
+         $("footer figcaption blockquote").toggle();
+         });
+         
+         $("footer figcaption ul li a").on('touchend',function(){
+         console.log(this);
+         $("footer figcaption blockquote").toggle();
+         });
+         */
+
+
+
+    });
+})(jQuery);
