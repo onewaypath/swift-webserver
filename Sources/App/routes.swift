87abefa9e45adb4ec77b48e4c696409ec75aa648
Alex Young <ayoung@onewaypath.com>
2020-03-18 18:38:40 -0400

    remove unixTools dependency

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -14,12 +14,12 @@ public func routes(_ router: Router) throws {
     
     
     
-    let edgehill3Html = unixTools().runUnix("cat", arguments: ["Public/edgehill-3.html"])
-    let bioayoungHtml = unixTools().runUnix("cat", arguments: ["Public/bio-ayoung.html"])
-    let biomyoungHtml = unixTools().runUnix("cat", arguments: ["Public/bio-myoung.html"])
-    let bioibsantosHtml = unixTools().runUnix("cat", arguments: ["Public/bio-ibsantos.html"])
-    let biomatgimeHtml = unixTools().runUnix("cat", arguments: ["Public/bio-matgime.html"])
-    let owpgmainHtml = unixTools().runUnix("cat", arguments: ["Public/owpg-main.html"])
+    let edgehill3Html = runUnix("cat", arguments: ["Public/edgehill-3.html"])
+    let bioayoungHtml = runUnix("cat", arguments: ["Public/bio-ayoung.html"])
+    let biomyoungHtml = runUnix("cat", arguments: ["Public/bio-myoung.html"])
+    let bioibsantosHtml = runUnix("cat", arguments: ["Public/bio-ibsantos.html"])
+    let biomatgimeHtml = runUnix("cat", arguments: ["Public/bio-matgime.html"])
+    let owpgmainHtml = runUnix("cat", arguments: ["Public/owpg-main.html"])
   /*
     router.get("subscribe") { req -> Future<View> in
         
@@ -164,4 +164,28 @@ struct outPut: Content {
 }
 */
 
-
+public func runUnix(_ command: String, commandPath: String = "/bin/", arguments: [String] = []) -> String {
+    
+    // Create a process (was NSTask on swift pre 3.0)
+    let task = Process()
+    let path = commandPath + command
+    
+    // Set the task parameters
+    task.launchPath = path
+    task.arguments = arguments
+    
+    // Create a Pipe and make the task
+    // put all the output there
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    
+    // Launch the task
+    task.launch()
+    
+    // Get the data
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: String.Encoding.utf8)
+    
+    return (output!)
+    
+}
