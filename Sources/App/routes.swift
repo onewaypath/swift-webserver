e335eb37b0dd651487181146da306e6a021e37ea
Alex Young <ayoung@onewaypath.com>
2020-03-19 19:40:17 -0400

    add do and catch

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -26,10 +26,18 @@ public func routes(_ router: Router) throws {
     
     // render the view for any html page that is identified
     router.get(String.parameter) { req -> Future<View> in
-        let htmlFile = try req.parameters.next(String.self)
-        let htmlFilePath = "Public/\(htmlFile)"
-        let html = unixTools().runUnix("cat", arguments: [htmlFilePath])
-        return try req.view().render("main-template", ["html": html])
+        
+        do {
+            let htmlFile = try req.parameters.next(String.self)
+            let htmlFilePath = "Public/\(htmlFile)"
+            let html = unixTools().runUnix("cat", arguments: [htmlFilePath])
+            return try req.view().render("main-template", ["html": html])
+        }
+        catch {
+            return try req.view().render("main-template", ["html": owpgmainHtml])
+        }
+        
+        
     }
 }
 
