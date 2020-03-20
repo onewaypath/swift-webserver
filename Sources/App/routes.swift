edceb68a54d8d89ee9f2624822404b6b623fd836
Alex Young <ayoung@onewaypath.com>
2020-03-19 21:01:08 -0400

    change to root directory refs

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -68,12 +68,12 @@ public func routes(_ router: Router) throws {
     */
     
     // render the view for any html page that is identified
-    router.get("owpg",String.parameter) { req -> Future<View> in
+    router.get(String.parameter) { req -> Future<View> in
         
         
            
             let htmlFile = try req.parameters.next(String.self)
-            let htmlFilePath = "Public/owpg/\(htmlFile)"
+            let htmlFilePath = "Public/\(htmlFile)"
             let html = unixTools().runUnix("cat", arguments: [htmlFilePath])
             return try req.view().render("main-template", ["html": html])
     
